d718ca79230f38af2a52ddac92025855ead2bcfc
azoz1 <Abdulaziz.Alhothali.jed.k1@tuwaiq.edu.sa>
2022-07-22 19:14:25 +0300

    change background programmatically and binding adapter

--- app/src/main/java/com/example/sallachallenge/models/DevelopersJson.kt
+++ app/src/main/java/com/example/sallachallenge/models/DevelopersJson.kt
@@ -1,13 +1,8 @@
 package com.example.sallachallenge.models
 
-import com.google.gson.annotations.SerializedName
 
 data class DevelopersJson(
-    @SerializedName("id")
     var id: String,
-    @SerializedName("store")
     var store: String,
-    @SerializedName("app_color")
     var app_color: String,
-    @SerializedName("font_family")
     var font_family: String)
